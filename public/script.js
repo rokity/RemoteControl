fed2b303533c35660c891bd7fdc410d404bbd7b6
Riccardo Amadio <laemaildiriccardo@gmail.com>
2016-05-30 19:11:26 +0200

    stable version

--- public/script.js
+++ public/script.js
@@ -1,35 +1,35 @@
-$(".up").mouseup(function(){
-  $(".up").removeClass('pressed');
+$("#up").mouseup(function(){
+  $("#up").removeClass('pressed');
 });
-$(".left").mouseup(function(){
-  $(".left").removeClass('pressed');
+$("#left").mouseup(function(){
+  $("#left").removeClass('pressed');
 });
-$(".down").mouseup(function(){
-  $(".down").removeClass('pressed');
+$("#down").mouseup(function(){
+  $("#down").removeClass('pressed');
 });
-$(".right").mouseup(function(){
-  $(".right").removeClass('pressed');
+$("#right").mouseup(function(){
+  $("#right").removeClass('pressed');
 });
 
-$(".up").mousedown(function(){
-  $(".up").addClass('pressed');
+$("#up").mousedown(function(){
+  $("#up").addClass('pressed');
   var socket=io();
   socket.emit('k', "up");
 });
 
-$(".left").mousedown(function(){
-  $(".left").addClass('pressed');
+$("#left").mousedown(function(){
+  $("#left").addClass('pressed');
   var socket=io();
   socket.emit('k', "left");
 
 });
-$(".down").mousedown(function(){
-  $(".down").addClass('pressed');
+$("#down").mousedown(function(){
+  $("#down").addClass('pressed');
   var socket=io();
   socket.emit('k', "down");
 });
-$(".right").mousedown(function(){
-  $(".right").addClass('pressed');
+$("#right").mousedown(function(){
+  $("#right").addClass('pressed');
   var socket=io();
   socket.emit('k', "right");
 });
